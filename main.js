196918123f4e799af0f98253b5533d2fb89c1ee8
exitdump <sheikhforid1026@gmail.com>
2020-08-17 23:39:21 +0600

    final update

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const searchBtn = document.getElementById('searchBtn');
-const inputMusic = document.getElementById("inputField");
+const inputSong = document.getElementById("inputField");
 
 
-inputMusic.addEventListener('keypress', function(){
+inputSong.addEventListener('keypress', function(){
     if(event.keyCode === 13){
         searchMusic();
     }
@@ -13,30 +13,30 @@ searchBtn.addEventListener('click', function(){
 });
 
 function searchMusic(){
-    document.getElementById('allResult').innerHTML = "";
-        document.getElementById('lyrics').innerHTML = "";
-     const keyword = inputMusic.value;    
+    document.getElementById('info').innerHTML = "";
+    document.getElementById('lyrics').innerHTML = "";
+     const keyword = inputSong.value;    
      fetch(`https://api.lyrics.ovh/suggest/${keyword}`)
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data=>{
-        storedData = data;
+        getdata = data;
         console.log(data);
         for (let i = 0; i < data.data.length; i++) {
             const title = data.data[i].title;
             const albumName = data.data[i].album.title;
             const artistName = data.data[i].artist.name;
             const id = data.data[i].id;
-            console.log(title);
-            document.getElementById('allResult').innerHTML += `<div class="single-result row align-items-center my-3 p-3">
-            <div class="col-md-6">
-                <h3 class="lyrics-name">${title}</h3>
-                <p class="author lead">Album by <span>${albumName}</span></p>
-                <p class="author lead">Artist by <a href=""> ${artistName}</a></p>
-            </div>
-            <div class="col-md-6 text-md-right text-center">
-                <a href="#lyrics-or-details"><button onClick="getLyrics(${id})" class="btn btn-success">Get Lyrics</button></a>
-            </div>
-        </div>`
+            document.getElementById('info').innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                                                            <div class="col-md-6">
+                                                                <h3 class="lyrics-name">${title}</h3>
+                                                                <p class="author lead">Album by <span>${albumName}</span></p>
+                                                                <p class="author lead">Artist by <a href=""> ${artistName}</a></p>
+                                                                <a href="#"><button onClick="preview(${id})" class="btn btn-success">Preview</button></a>
+                                                            </div>
+                                                            <div class="col-md-6 text-md-right text-center">
+                                                                <a href="#"><button onClick="getLyrics(${id})" class="btn btn-success">Get Lyrics</button></a>
+                                                            </div>
+                                                        </div>`
             
 
             if(i == 9){
@@ -48,20 +48,20 @@ function searchMusic(){
 
 function getLyrics(id){
     for (let i = 0; i < 10; i++) {
-        if(storedData.data[i].id == id){
-            const artistName = storedData.data[i].artist.name;
-            const songTitle = storedData.data[i].title;
+        if(getdata.data[i].id === id){
+            const artistName = getdata.data[i].artist.name;
+            const songTitle = getdata.data[i].title;
             fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
-            .then(res => res.json())
+            .then(response => response.json())
             .then(data => {
                 let lyrics = data.lyrics;
-                if(lyrics == undefined){
-                    lyrics = `Song Lyrics Not Found. Try for another song`;
+                if(lyrics === undefined){
+                    lyrics = `Sorry we cann't found this song Lyrics. Please try another song`;
                 }
                 document.getElementById('lyrics').innerHTML = `<div class="single-lyrics text-center">
                                                                             <button class="btn go-back">&lsaquo;</button>
-                                                                            <h2 class="text-success mb-4">Song Lyrics</h2>
-                                                                            <h5>${lyrics}</h5>
+                                                                            <h2 class="text-success mb-4">Lyrics</h2>
+                                                                            <h5 class="text-muted">${lyrics}</h5>
                                                                         </div>`
             })
             
@@ -69,4 +69,26 @@ function getLyrics(id){
         }
     }
     
+}
+
+ function preview(id){
+    for(let i = 0; i < 10; i++){
+        if(getdata.data[i].id === id){
+            const artistName = getdata.data[i].artist.name;
+            const songTitle = getdata.data[i].title;
+            const coverPhoto = getdata.data[i].album.cover;
+            const trackLink = getdata.data[i].link;
+            const duration =  getdata.data[i].duration/60;
+            const getDuration = duration.toFixed(2);
+            const downloadPreview = getdata.data[i].preview;  
+            document.getElementById('info').innerHTML += `<div class="preview text-center mt-5">
+                                                                        <h3 class="text-success mb-4">Preview</h3>
+                                                                        <img src="${coverPhoto}" alt="" class="mb-2">
+                                                                        <h5>Song Title: ${songTitle}</h5>
+                                                                        <h5>Artist Name: ${artistName}</h5>
+                                                                        <h5><a target="_blank" href="${trackLink}">Click here to get track link</a></h5>
+                                                                        <h5>Duration: ${getDuration}s</h5>
+                                                                        <h5><a target="_blank" href="${downloadPreview}">Click here to download preview</a></h5>                                                                   </div>`
+        }
+    }
 }
